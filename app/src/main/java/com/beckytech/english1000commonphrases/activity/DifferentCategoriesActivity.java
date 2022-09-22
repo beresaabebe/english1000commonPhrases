d26daf4bf2db71c4eafd88c68d95268483d8a5e9
Becky <roobaanuuf@gmail.com>
2022-09-22 12:07:33 +0300

    save all

--- app/src/main/java/com/beckytech/english1000commonphrases/activity/DifferentCategoriesActivity.java
+++ app/src/main/java/com/beckytech/english1000commonphrases/activity/DifferentCategoriesActivity.java
@@ -1,39 +1,27 @@
 package com.beckytech.english1000commonphrases.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.beckytech.english1000commonphrases.R;
-import com.beckytech.english1000commonphrases.adapter.Adapter;
 import com.beckytech.english1000commonphrases.adapter.AdapterDetail;
-import com.beckytech.english1000commonphrases.contents.CategoryContent;
 import com.beckytech.english1000commonphrases.contents.ContentDetail;
-import com.beckytech.english1000commonphrases.contents.SubTitleContent;
-import com.beckytech.english1000commonphrases.contents.TitleContent;
-import com.beckytech.english1000commonphrases.model.Model;
 import com.beckytech.english1000commonphrases.model.ModelDetail;
 import com.beckytech.english1000commonphrases.model.ModelViewPager;
-import com.google.android.gms.ads.interstitial.InterstitialAd;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class DifferentCategoriesActivity extends AppCompatActivity {
 
     private ModelViewPager modelViewPager;
     private List<ModelDetail> modelList;
-    private final TitleContent titleContent = new TitleContent();
-    private final SubTitleContent subTitleContent = new SubTitleContent();
     private final ContentDetail contentDetail = new ContentDetail();
-    private final CategoryContent categoryContent = new CategoryContent();
-    private InterstitialAd mInterstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,14 +38,11 @@ public class DifferentCategoriesActivity extends AppCompatActivity {
         TextView rev_title_different_categories = findViewById(R.id.rev_title_different_categories);
 
         String receivedValue = modelViewPager.getTag().toLowerCase();
-        StringBuilder stringBuilder = new StringBuilder(receivedValue);
-        stringBuilder = stringBuilder.deleteCharAt(receivedValue.length() - 1);
-        stringBuilder = stringBuilder.deleteCharAt(0);
 
-        title.setText(String.format("Useful Phrases of %s%s words", stringBuilder.substring(0, 1).toUpperCase(), stringBuilder.substring(1)));
+        title.setText(String.format("Useful Phrases of %s%s words", receivedValue.substring(0, 1).toUpperCase(), receivedValue.substring(1)));
 
-        rev_title_different_categories.setText(String.format("Phrases of %s", stringBuilder.substring(0,1).toUpperCase()+""+
-                stringBuilder.substring(1)));
+        rev_title_different_categories.setText(String.format("Phrases of %s", receivedValue.substring(0,1).toUpperCase()+""+
+                receivedValue.substring(1)));
         getDataOfContentDetail();
 
         AdapterDetail adapterDetail = new AdapterDetail(this, modelList);
