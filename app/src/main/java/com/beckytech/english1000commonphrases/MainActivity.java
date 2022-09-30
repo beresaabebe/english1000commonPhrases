6ad78ab8ff3340125634a30769ffcaf7fa66dbf0
Becky <roobaanuuf@gmail.com>
2022-09-30 20:09:41 +0300

    save

--- app/src/main/java/com/beckytech/english1000commonphrases/MainActivity.java
+++ app/src/main/java/com/beckytech/english1000commonphrases/MainActivity.java
@@ -100,7 +100,7 @@ public class MainActivity extends AppCompatActivity implements Adapter.onClicked
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         getData();
         adapter = new Adapter(list, this);
-        Collections.sort(list, adapter.sort);
+//        Collections.sort(list, adapter.sort);
         recyclerView.setAdapter(adapter);
 
         ViewPager2 viewPager2 = findViewById(R.id.viewPager_main);
